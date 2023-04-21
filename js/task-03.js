46c4772a003491e0cf4bdd838d29d8ce81b24c88
go-to-stars <121138297+go-to-stars@users.noreply.github.com>
2023-04-21 13:57:18 +0300

    added a script for the third task

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,55 @@
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+// 1. Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// 2. Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// 3. Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const imagesList = images
+  .map(
+    (element) =>
+      `<li class="gallery-list-item"><img class="gallery-list-item-img" src="${element.url}" alt="${element.alt}" width="400"></li>`
+  )
+  .join("");
+
+const imagesGallery = document.querySelector(".gallery");
+
+imagesGallery.insertAdjacentHTML("afterbegin", imagesList); // додати в DOM за одну операцію
+
+// оформлення галереї флексбоксами
+imagesGallery.style.listStyle = "none";
+imagesGallery.style.display = "flex";
+imagesGallery.style.justifyContent = "center";
+imagesGallery.style.gap = "20px";
+imagesGallery.style.margin = "0px";
+imagesGallery.style.padding = "0px";
+
+// оформлення сітки
+const imagesGalleryItem = document
+  .querySelectorAll(".gallery-list-item")
+  .forEach((value) => {
+    value.style.flexBasis = "calc((100% - 40px) / 3)";
+  });
+
+// оформлення стилів малюнку
+const imagesGalleryItemImg = document
+  .querySelectorAll(".gallery-list-item-img")
+  .forEach((value) => {
+    value.style.display = "block";
+    value.style.width = "100%";
+    value.style.height = "100%";
+    value.style.objectFit = "cover";
+  });
