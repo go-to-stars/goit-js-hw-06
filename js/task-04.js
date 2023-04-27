f09e0469ff1967bb0777892efb8d36a1a2bf1cc1
go-to-stars <121138297+go-to-stars@users.noreply.github.com>
2023-04-27 18:34:11 +0300

    added changes (option 2) according to the mentor's recommendations

--- js/task-04.js
+++ js/task-04.js
@@ -3,20 +3,46 @@
 // 2. Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // 3. Оновлюй інтерфейс новим значенням змінної counterValue.
 
+// Варіант 1 ----------------------------------------------
+
+// let counterValue = 0;
+
+// const value = document.querySelector("#value");
+
+// const subtractBtn = document.querySelector("button[data-action=decrement]");
+
+// // оновлення інтерфейсу новим значенням змінної counterValue по події "click -"
+// subtractBtn.addEventListener("click", () => {
+//   value.innerHTML = `<span id="value">${(counterValue -= 1)}</span>`;
+// });
+
+// const addBtn = document.querySelector("button[data-action=increment]");
+
+// // оновлення інтерфейсу новим значенням змінної counterValue по події "click +"
+// addBtn.addEventListener("click", () => {
+//   value.innerHTML = `<span id="value">${(counterValue += 1)}</span>`;
+// });
+
+// Варіант 2 (рекомендація ментора) ----------------------------------------------
+
 let counterValue = 0;
 
 const value = document.querySelector("#value");
 
 const subtractBtn = document.querySelector("button[data-action=decrement]");
+const addBtn = document.querySelector("button[data-action=increment]");
+
+// функція зміни значення лічильника
+function changeTextContent(val) {
+  value.textContent = val;
+}
 
 // оновлення інтерфейсу новим значенням змінної counterValue по події "click -"
 subtractBtn.addEventListener("click", () => {
-  value.innerHTML = `<span id="value">${(counterValue -= 1)}</span>`;
+  changeTextContent((counterValue -= 1));
 });
-
-const addBtn = document.querySelector("button[data-action=increment]");
 
 // оновлення інтерфейсу новим значенням змінної counterValue по події "click +"
 addBtn.addEventListener("click", () => {
-  value.innerHTML = `<span id="value">${(counterValue += 1)}</span>`;
+  changeTextContent((counterValue += 1));
 });
