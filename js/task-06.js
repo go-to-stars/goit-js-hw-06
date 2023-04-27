ce25d42888935377c9a24da4262e5402f9aff340
go-to-stars <121138297+go-to-stars@users.noreply.github.com>
2023-04-27 18:42:37 +0300

    added the function of adding and cleaning classes according to the mentor's recommendations

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,24 @@
 // 2. Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
 // 3. Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
+// Варіант 1 ----------------------------------------------
+// const textInput = document.querySelector("#validation-input");
+
+// textInput.addEventListener("blur", (e) => {
+//   e.currentTarget.value.length === Number(textInput.getAttribute("data-length"))
+//     ? (textInput.classList.add("valid"), textInput.classList.remove("invalid"))
+//     : (textInput.classList.add("invalid"), textInput.classList.remove("valid"));
+// });
+
+// Варіант 2 (рекомендація ментора) ----------------------------------------------
 const textInput = document.querySelector("#validation-input");
+
+function addRemoveClassList(add, remove) {
+  textInput.classList.add(add), textInput.classList.remove(remove);
+}
 
 textInput.addEventListener("blur", (e) => {
   e.currentTarget.value.length === Number(textInput.getAttribute("data-length"))
-    ? (textInput.classList.add("valid"), textInput.classList.remove("invalid"))
-    : (textInput.classList.add("invalid"), textInput.classList.remove("valid"));
+    ? addRemoveClassList("valid", "invalid")
+    : addRemoveClassList("invalid", "valid");
 });
